b0b9c939e755bd96528f5a913f93713f0dc0ed3c
Guilouf <Guilouf@users.noreply.github.com>
2019-11-01 12:53:12 +0100

    Typo on Jos name

--- fluid_sim.py
+++ fluid_sim.py
@@ -1,5 +1,5 @@
 """
-Based on the Joe Stam paper https://www.researchgate.net/publication/2560062_Real-Time_Fluid_Dynamics_for_Games
+Based on the Jos Stam paper https://www.researchgate.net/publication/2560062_Real-Time_Fluid_Dynamics_for_Games
 and the mike ash vulgarization https://mikeash.com/pyblog/fluid-simulation-for-dummies.html
 """
 import numpy as np
